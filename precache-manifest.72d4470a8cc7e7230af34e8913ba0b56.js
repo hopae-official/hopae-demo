44d2f3b73f4c8814cbe51d0975c941744367d3bc
Pensivej <jshim10@illinois.edu>
2019-12-16 20:54:36 +0900

    Updates

---
+++ precache-manifest.72d4470a8cc7e7230af34e8913ba0b56.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "66e7f9d6d0c722e3ce37785476030c0d",
+    "url": "/jeju/static/media/dummy-content-3.66e7f9d6.svg"
+  },
+  {
+    "revision": "490896e3f08e5c69ecfc",
+    "url": "/jeju/static/css/main.5d57255d.chunk.css"
+  },
+  {
+    "revision": "46eb46e6a55783878cb7",
+    "url": "/jeju/static/js/runtime~main.e4e8fcfa.js"
+  },
+  {
+    "revision": "4798175efa46bd3dde35",
+    "url": "/jeju/static/js/2.ed8ff8f0.chunk.js"
+  },
+  {
+    "revision": "09cfe256212b76eed2c5e1cbde567ed0",
+    "url": "/jeju/static/media/loading.09cfe256.svg"
+  },
+  {
+    "revision": "6b9bf7bb2608ab3ff694cb65554fffd6",
+    "url": "/jeju/static/media/uport-logo.6b9bf7bb.svg"
+  },
+  {
+    "revision": "37b638cc8f9e0253f12bf430911c028b",
+    "url": "/jeju/static/media/arrow-left.37b638cc.svg"
+  },
+  {
+    "revision": "baee24254676badffe8a3e9df237a794",
+    "url": "/jeju/static/media/itunes.baee2425.svg"
+  },
+  {
+    "revision": "00fc1c084d9ac202886c1768dbdd76df",
+    "url": "/jeju/static/media/playstore.00fc1c08.svg"
+  },
+  {
+    "revision": "066d8846ccbbc22a75bafbf6c89a6a9b",
+    "url": "/jeju/static/media/reload.066d8846.svg"
+  },
+  {
+    "revision": "a6d8b79cf6df680193b67a52f0a66418",
+    "url": "/jeju/static/media/jeju.a6d8b79c.png"
+  },
+  {
+    "revision": "6cfca76ff30bbf5ceb4ceeb0c6574d1e",
+    "url": "/jeju/static/media/uport-icon-white.6cfca76f.svg"
+  },
+  {
+    "revision": "89b797181803066f19b238f8c41c625c",
+    "url": "/jeju/static/media/university.89b79718.png"
+  },
+  {
+    "revision": "7563f6ed5f6bf6b6c678cf0762c1db76",
+    "url": "/jeju/static/media/insurance.7563f6ed.png"
+  },
+  {
+    "revision": "8d28001e0891ddeaf5492dc76681d8da",
+    "url": "/jeju/static/media/wave.8d28001e.svg"
+  },
+  {
+    "revision": "2824005735918aa127003ee4cd246975",
+    "url": "/jeju/static/media/dummy-content-1.28240057.svg"
+  },
+  {
+    "revision": "4547cb485be11921726db30452f257bc",
+    "url": "/jeju/static/media/dummy-content-2.4547cb48.svg"
+  },
+  {
+    "revision": "490896e3f08e5c69ecfc",
+    "url": "/jeju/static/js/main.8cec1fa0.chunk.js"
+  },
+  {
+    "revision": "f5a9f90c7cdf78be5c1f28f6b115e83c",
+    "url": "/jeju/static/media/dummy-content-4.f5a9f90c.svg"
+  },
+  {
+    "revision": "fee7033cfa58536bdbca954d9594dfa1",
+    "url": "/jeju/static/media/city-id-icon.fee7033c.svg"
+  },
+  {
+    "revision": "4ec3278f3a02cab1a30c575990d80fbb",
+    "url": "/jeju/static/media/uport-app-icon.4ec3278f.svg"
+  },
+  {
+    "revision": "18cf63216ed12ed2dd1803618239ae68",
+    "url": "/jeju/static/media/services-icon.18cf6321.svg"
+  },
+  {
+    "revision": "df53d6421dd342cfa55f0a3251c941bc",
+    "url": "/jeju/static/media/popup-arrow.df53d642.svg"
+  },
+  {
+    "revision": "cfb15947b844ede9ded5b8af94b53b36",
+    "url": "/jeju/static/media/handy-phone.cfb15947.svg"
+  },
+  {
+    "revision": "732b78bd2816d80ae31fe216391bd450",
+    "url": "/jeju/static/media/angle-icon.732b78bd.svg"
+  },
+  {
+    "revision": "5a6ec01cf762d6ec1107a9b5ae482dee",
+    "url": "/jeju/static/media/secure-icon.5a6ec01c.svg"
+  },
+  {
+    "revision": "2d30e218d759bd6c599c4e0ea3a9d75b",
+    "url": "/jeju/static/media/smiley-face-city.2d30e218.svg"
+  },
+  {
+    "revision": "f94a720623c83ac97cdc2bbe14d9eded",
+    "url": "/jeju/static/media/accept-attestation.f94a7206.svg"
+  },
+  {
+    "revision": "57ba07c42b2ade791c634ae159c51da4",
+    "url": "/jeju/static/media/congratulations-city.57ba07c4.svg"
+  },
+  {
+    "revision": "1cc7f43c0cd3ac75d3d1f8ba0a5df04e",
+    "url": "/jeju/static/media/dev-icon.1cc7f43c.svg"
+  },
+  {
+    "revision": "4a871205c5e9fd5734803cfe54b2125f",
+    "url": "/jeju/static/media/citizen-icon.4a871205.svg"
+  },
+  {
+    "revision": "e5f8473981cf1cd41b118d504dd9bd7c",
+    "url": "/jeju/static/media/grumpy-face.e5f84739.svg"
+  },
+  {
+    "revision": "ee6b2d9776d0c71268c09df98079a56d",
+    "url": "/jeju/static/media/smiley-face-diploma.ee6b2d97.svg"
+  },
+  {
+    "revision": "0d9ffac3d65007d94e61005ecc17e358",
+    "url": "/jeju/index.html"
+  }
+];
